2711898aaf8e52df414dd64b7d9b5ce0fca68dc0
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 21:02:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,11 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, data|
+    data.each do |holiday, supplies|
+      puts "#{season.capitalize}: <br/>" 
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
