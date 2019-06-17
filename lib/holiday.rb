957539373c8a6329a4ef1f787ab6192563c08fc3
Ursula Miller <ursula.miller@gmail.com>
2019-06-17 21:19:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"   
     data.each do |holiday, supplies|
       holiday = holiday.to_s
-      holiday = holiday.titleize
+      holiday = holiday.
       holiday = holiday.sub("_", " ")
       puts "  #{holiday}: #{supplies.join(", ")}" 
     end
